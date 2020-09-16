5f65cd3ad7df09a72f966a63a1579c8281a9f85d
Gabriel Daenner <gabrieldaenner@googlemail.com>
2020-09-16 16:18:05 +0200

    Prettified cpp code

--- src/JNIHelper.cpp
+++ src/JNIHelper.cpp
@@ -38,7 +38,7 @@
 
     typedef struct beep_head
     {
-        int frame;
+        int        frame;
         beep_note* note;
     } beep_head;
 #endif
@@ -114,7 +114,7 @@ int generateNewColorCode(WORD currentConsoleAttr, WORD newConsoleAttr)
     strncpy(hexCurrent, decToHex(currentConsoleAttr), 3);
     strncpy(hexNew,     decToHex(newConsoleAttr),     3);
 
-    if(strlen(hexCurrent) == 1)
+    if (strlen(hexCurrent) == 1)
     {
         hexCurrent[0] = hexNew[0];
         hexCurrent[1] = hexNew[1];
@@ -133,14 +133,14 @@ int generateNewColorCode(WORD currentConsoleAttr, WORD newConsoleAttr)
 #ifdef __linux__
 void portaudio_initialize()
 {
-    if(!freopen("/dev/null", "w", stderr))
+    if (!freopen("/dev/null", "w", stderr))
     {
         printf("%s", "Failed to redirect stderr to /dev/null\n");
     }
 
     Pa_Initialize();
 
-    if(!freopen("/dev/tty", "w", stderr))
+    if (!freopen("/dev/tty", "w", stderr))
     {
         printf("%s", "Failed to redirect stderr to /dev/tty\n");
     }
@@ -148,27 +148,27 @@ void portaudio_initialize()
 
 void portaudio_terminate()
 {
-    if(!freopen("/dev/null", "w", stderr))
+    if (!freopen("/dev/null", "w", stderr))
     {
         printf("%s", "Failed to redirect stderr to /dev/null\n");
     }
 
     Pa_Terminate();
 
-    if(!freopen("/dev/tty", "w", stderr))
+    if (!freopen("/dev/tty", "w", stderr))
     {
         printf("%s", "Failed to redirect stderr to /dev/tty\n");
     }
 }
 
-int beep_head_callback(const void *inputBuffer, void *outputBuffer,
+int beep_head_callback(const void* inputBuffer, void* outputBuffer,
     unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo* timeInfo,
-    PaStreamCallbackFlags statusFlags, void *userData)
+    PaStreamCallbackFlags statusFlags, void* userData)
 {
     beep_head* head   = (beep_head*)(userData);
     float*     buffer = (float*)outputBuffer;
 
-    for(int frame = 0; frame < framesPerBuffer; frame++)
+    for (int frame = 0; frame < framesPerBuffer; frame++)
     {
         buffer[frame] = head->note->waveform(head);
         head->frame++;
@@ -193,18 +193,20 @@ void beep_note_play(beep_note* note)
     Pa_AbortStream(stream);
 }
 
-float beep_waveform_sine(beep_head *head)
+float beep_waveform_sine(beep_head* head)
 {
-   beep_note *note = head->note;
-   return note->amplitude * sin(2 * M_PI * note->frequency * head->frame / SAMPLE_RATE);
+    beep_note* note = head->note;
+    return note->amplitude * sin(2 * M_PI * note->frequency
+        * head->frame / SAMPLE_RATE);
 }
 #endif
 
 //core methods
-JNIEXPORT jdouble JNICALL Java_JNIHelper_getSystemMemoryInfo(JNIEnv *env, jclass javaClass, jstring type, jstring unitMode, jboolean round)
+JNIEXPORT jdouble JNICALL Java_JNIHelper_getSystemMemoryInfo(JNIEnv* env,
+    jclass javaClass, jstring type, jstring unitMode, jboolean round)
 {
-    const char *t  = env->GetStringUTFChars(type, NULL);
-    const char *um = env->GetStringUTFChars(unitMode, NULL);
+    const char* t  = env->GetStringUTFChars(type, NULL);
+    const char* um = env->GetStringUTFChars(unitMode, NULL);
 
     double  divider          = 0;
     jdouble systemMemoryInfo = 0;
@@ -267,7 +269,8 @@ JNIEXPORT jdouble JNICALL Java_JNIHelper_getSystemMemoryInfo(JNIEnv *env, jclass
     return systemMemoryInfo;
 }
 
-JNIEXPORT void JNICALL Java_JNIHelper_beep(JNIEnv *env, jclass javaClass, jint frequency, jint duration)
+JNIEXPORT void JNICALL Java_JNIHelper_beep(JNIEnv* env, jclass javaClass,
+    jint frequency, jint duration)
 {
     #ifdef _WIN32
     Beep(frequency, duration);
@@ -275,10 +278,10 @@ JNIEXPORT void JNICALL Java_JNIHelper_beep(JNIEnv *env, jclass javaClass, jint f
     portaudio_initialize();
 
     beep_note note = {
-       .amplitude = 1.0,
-       .duration = (float)duration,
-       .frequency = (float)frequency,
-       .waveform = beep_waveform_sine
+       .amplitude  = 1.0,
+       .duration   = (float)duration,
+       .frequency  = (float)frequency,
+       .waveform   = beep_waveform_sine
     };
 
     beep_note_play(&note);
@@ -286,7 +289,8 @@ JNIEXPORT void JNICALL Java_JNIHelper_beep(JNIEnv *env, jclass javaClass, jint f
     #endif
 }
 
-JNIEXPORT void JNICALL Java_JNIHelper_consoleClear(JNIEnv *env, jclass javaClass)
+JNIEXPORT void JNICALL Java_JNIHelper_consoleClear(JNIEnv* env,
+    jclass javaClass)
 {
     flushBuffers();
 
@@ -295,28 +299,30 @@ JNIEXPORT void JNICALL Java_JNIHelper_consoleClear(JNIEnv *env, jclass javaClass
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     DWORD                      count;
     DWORD                      cellCount;
-    COORD                      homeCoords = { 0, 0 };
+    COORD                      homeCoords = {0, 0};
 
-    hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+    hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
     if (hStdOut == INVALID_HANDLE_VALUE)
     {
         return;
     }
 
-    if (!GetConsoleScreenBufferInfo( hStdOut, &csbi ))
+    if (!GetConsoleScreenBufferInfo(hStdOut, &csbi))
     {
         return;
     }
 
-    cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+    cellCount = csbi.dwSize.X * csbi.dwSize.Y;
 
-    if (!FillConsoleOutputCharacter(hStdOut, (TCHAR) ' ', cellCount, homeCoords, &count))
+    if (!FillConsoleOutputCharacter(hStdOut, (TCHAR) ' ', cellCount,
+        homeCoords, &count))
     {
         return;
     }
 
-    if (!FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, cellCount, homeCoords, &count))
+    if (!FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, cellCount,
+        homeCoords, &count))
     {
         return;
     }
@@ -341,7 +347,8 @@ JNIEXPORT void JNICALL Java_JNIHelper_consoleClear(JNIEnv *env, jclass javaClass
     flushBuffers();
 }
 
-JNIEXPORT void JNICALL Java_JNIHelper_consolePause(JNIEnv *env, jclass javaClass)
+JNIEXPORT void JNICALL Java_JNIHelper_consolePause(JNIEnv* env,
+    jclass javaClass)
 {
     flushBuffers();
 
@@ -353,9 +360,9 @@ JNIEXPORT void JNICALL Java_JNIHelper_consolePause(JNIEnv *env, jclass javaClass
     chKey  = getPressedCharacter();
     numKey = chKey;
 
-    for(int i = 1; i <= 256; i++)
+    for (int i = 1; i <= 256; i++)
     {
-        if(i == numKey)
+        if (i == numKey)
         {
             break;
         }
@@ -368,12 +375,13 @@ JNIEXPORT void JNICALL Java_JNIHelper_consolePause(JNIEnv *env, jclass javaClass
     flushBuffers();
 }
 
-JNIEXPORT void JNICALL Java_JNIHelper_printColoredText(JNIEnv *env, jclass javaClass, jstring str, jint colorCode)
+JNIEXPORT void JNICALL Java_JNIHelper_printColoredText(JNIEnv* env,
+    jclass javaClass, jstring str, jint colorCode)
 {
     flushBuffers();
 
-    const char *arg = env->GetStringUTFChars(str, NULL);
-    
+    const char* arg = env->GetStringUTFChars(str, NULL);
+
     #ifdef _WIN32
     HANDLE                     hConsole;
     WORD                       currentConsoleAttr;
@@ -381,7 +389,7 @@ JNIEXPORT void JNICALL Java_JNIHelper_printColoredText(JNIEnv *env, jclass javaC
 
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    if(GetConsoleScreenBufferInfo(hConsole, &csbi))
+    if (GetConsoleScreenBufferInfo(hConsole, &csbi))
     {
         currentConsoleAttr = csbi.wAttributes;
     }
@@ -392,7 +400,8 @@ JNIEXPORT void JNICALL Java_JNIHelper_printColoredText(JNIEnv *env, jclass javaC
     }
 
     FlushConsoleInputBuffer(hConsole);
-    SetConsoleTextAttribute(hConsole, generateNewColorCode(currentConsoleAttr, colorCode));
+    SetConsoleTextAttribute(hConsole,
+        generateNewColorCode(currentConsoleAttr, colorCode));
 
     printf("%s", arg);
 
@@ -406,9 +415,10 @@ JNIEXPORT void JNICALL Java_JNIHelper_printColoredText(JNIEnv *env, jclass javaC
     flushBuffers();
 }
 
-JNIEXPORT void JNICALL Java_JNIHelper_sendStringToNative(JNIEnv *env, jclass javaClass, jstring str)
+JNIEXPORT void JNICALL Java_JNIHelper_sendStringToNative(JNIEnv* env,
+    jclass javaClass, jstring str)
 {
-    const char *arg = env->GetStringUTFChars(str, NULL);
-    
+    const char* arg = env->GetStringUTFChars(str, NULL);
+
     system(arg);
 }
