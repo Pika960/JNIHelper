6344037066bb56d9f66e4867845f59d3f4b0bbb0
Gabriel Daenner <gabrieldaenner@googlemail.com>
2020-11-11 19:32:05 +0100

    Fixed some javadoc comments

--- src/HKEY.java
+++ src/HKEY.java
@@ -8,36 +8,36 @@ public enum HKEY
 {
     /**
      * Registry entries subordinate to this key define types (or classes)
-       of documents and the properties associated with those types.
-       Warning: Write requires elevated priviliges.
+     * of documents and the properties associated with those types.
+     * Warning: Write requires elevated priviliges.
      */
     HKEY_CLASSES_ROOT("HKEY_CLASSES_ROOT"),
 
     /**
      * Contains information about the current hardware profile of the local
-       computer system.
-       Warning: Write requires elevated priviliges.
+     * computer system.
+     * Warning: Write requires elevated priviliges.
      */
     HKEY_CURRENT_CONFIG("HKEY_CURRENT_CONFIG"),
 
     /**
      * Registry entries subordinate to this key define the preferences of the
-       current user.
+     * current user.
      */
     HKEY_CURRENT_USER("HKEY_CURRENT_USER"),
 
     /**
      * Registry entries subordinate to this key define the physical state of
-       the computer, including data about the bus type, system memory, and
-       installed hardware and software.
-       Warning: Write requires elevated priviliges.
+     * the computer, including data about the bus type, system memory, and
+     * installed hardware and software.
+     * Warning: Write requires elevated priviliges.
      */
     HKEY_LOCAL_MACHINE("HKEY_LOCAL_MACHINE"),
 
     /**
      * Registry entries subordinate to this key define the default user
-       configuration for new users on the local computer and the user
-       configuration for the current user.
+     * configuration for new users on the local computer and the user
+     * configuration for the current user.
      */
     HKEY_USERS("HKEY_USERS");
 
