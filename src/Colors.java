317536629650ddfca1eb581b6dc003ef0dbf3f1f
Gabriel Daenner <gabrieldaenner@googlemail.com>
2020-11-11 19:28:05 +0100

    Improved enum Colors

--- src/Colors.java
+++ src/Colors.java
@@ -9,113 +9,103 @@ public enum Colors
     /**
      * the color black
      */
-    BLACK,
+    BLACK(0, 30),
 
     /**
      * the color blue
      */
-    BLUE,
+    BLUE(1, 34),
 
     /**
      * the color green
      */
-    GREEN,
+    GREEN(2, 32),
 
     /**
      * the color aqua
      */
-    AQUA,
+    AQUA(3, 36),
 
     /**
      * the color red
      */
-    RED,
+    RED(4, 31),
 
     /**
      * the color purple
      */
-    PURPLE,
+    PURPLE(5, 35),
 
     /**
      * the color yellow
      */
-    YELLOW,
+    YELLOW(6, 33),
 
     /**
      * the color white
      */
-    WHITE,
+    WHITE(7, 37),
 
     /**
      * the color gray
      */
-    GRAY,
+    GRAY(8, 37),
 
     /**
      * the color light blue
      */
-    LIGHT_BLUE,
+    LIGHT_BLUE(9, 34),
 
     /**
      * the color light green
      */
-    LIGHT_GREEN,
+    LIGHT_GREEN(10, 32),
 
     /**
      * the color light aqua
      */
-    LIGHT_AQUA,
+    LIGHT_AQUA(11, 36),
 
     /**
      * the color light red
      */
-    LIGHT_RED,
+    LIGHT_RED(12, 31),
 
     /**
      * the color light purple
      */
-    LIGHT_PURPLE,
+    LIGHT_PURPLE(13, 35),
 
     /**
      * the color light yellow
      */
-    LIGHT_YELLOW,
+    LIGHT_YELLOW(14, 33),
 
     /**
      * the color bright white
      */
-    BRIGHT_WHITE;
+    BRIGHT_WHITE(15, 37);
+
+    private final int    colorCodeWindows;
+    private final int    colorCodeUnix;
+    private final String systemName;
+
+    private Colors(int colorCodeWindows, int colorCodeUnix)
+    {
+        this.colorCodeWindows = colorCodeWindows;
+        this.colorCodeUnix    = colorCodeUnix;
+        this.systemName = JNIHelper.getOperatingSystemName().toLowerCase();
+    }
 
     /**
-     * assigns a numeric value to the enum
+     * get the platform-specific color code
      * @return the platform-specific color code
      */
     public int getColorCode()
     {
-        String systemName = JNIHelper.getOperatingSystemName().toLowerCase();
-
         if(systemName.contains("win"))
         {
-            switch(this)
-            {
-                case BLACK:        return  0;
-                case BLUE:         return  1;
-                case GREEN:        return  2;
-                case AQUA:         return  3;
-                case RED:          return  4;
-                case PURPLE:       return  5;
-                case YELLOW:       return  6;
-                case WHITE:        return  7;
-                case GRAY:         return  8;
-                case LIGHT_BLUE:   return  9;
-                case LIGHT_GREEN:  return 10;
-                case LIGHT_AQUA:   return 11;
-                case LIGHT_RED:    return 12;
-                case LIGHT_PURPLE: return 13;
-                case LIGHT_YELLOW: return 14;
-                case BRIGHT_WHITE: return 15;
-                default:           return  0;
-            }
+            return colorCodeWindows;
         }
 
         else if(systemName.contains("aix") || systemName.contains("bsd")
@@ -123,26 +113,7 @@ public enum Colors
             || systemName.contains("nix")  || systemName.contains("nux")
             || systemName.contains("sol"))
         {
-            switch(this)
-            {
-                case BLACK:        return 30;
-                case BLUE:         return 34;
-                case GREEN:        return 32;
-                case AQUA:         return 36;
-                case RED:          return 31;
-                case PURPLE:       return 35;
-                case YELLOW:       return 33;
-                case WHITE:        return 37;
-                case GRAY:         return 37;
-                case LIGHT_BLUE:   return 34;
-                case LIGHT_GREEN:  return 32;
-                case LIGHT_AQUA:   return 36;
-                case LIGHT_RED:    return 31;
-                case LIGHT_PURPLE: return 35;
-                case LIGHT_YELLOW: return 33;
-                case BRIGHT_WHITE: return 37;
-                default:           return 30;
-            }
+            return colorCodeUnix;
         }
 
         else
